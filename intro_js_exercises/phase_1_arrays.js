868109ac5dbdcbb74deaece04f69c991faa83199
Brandon Choi <bchoi28@gmail.com>
2023-01-04 20:34:52 -0500

    two_sum

--- intro_js_exercises/phase_1_arrays.js
+++ intro_js_exercises/phase_1_arrays.js
@@ -10,10 +10,20 @@ Array.prototype.uniq = function () {
     return uniqueArray;
 };
 
-console.log([1, 1, 2, 2, 3, 3, 4, 4, 5, 5].uniq());
+console.log([1, 1, 2, 2, 3, 3].uniq()); // => [1,2,3]
 
 Array.prototype.twoSum = function () {
+    const pairs = [];
 
+    for (let i = 0; i < this.length; i++) {
+        for (let j = (i + 1); j < this.length; j++) {
+            if (this[i] + this[j] === 0) {
+                pairs.push([i, j]);
+            }
+        }
+    }
 
-
+    return pairs;
 };
+
+console.log([-1, 0, -3, 3, 2, 1].twoSum()); // => [[0,5],[2,3]]
